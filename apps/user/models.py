48c32b63ceb498cb5b5d6da871fe310c7b7ac4a7
Yoiner Duran <duranyoiner@gmail.com>
2023-06-07 21:31:56 -0500

    mostrar foto de perfil

--- apps/user/models.py
+++ apps/user/models.py
@@ -5,7 +5,7 @@ class User(AbstractUser):
     email = models.EmailField(null=False, blank=False,unique=True,verbose_name='Correo electrónico')
     imagen=models.ImageField(upload_to=f'{MEDIA_URL}users/%Y/%m/',null=True,blank=True, verbose_name='Foto de perfil')
 
-    def get_image(self):
+    def get_imagen(self):
         if self.imagen:
-            return f'{MEDIA_URL}{self.imagen}'
+            return self.imagen.url
         return f'{STATIC_URL}media/img/empty.png'
